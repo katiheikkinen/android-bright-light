12a894de15d9f312ff5b58995338a58dec6f5957
Kati Heikkinen <kati.a.heikkinen@gmail.com>
2019-07-31 17:46:50 +0300

    Initial layout created with bottom nav

---
+++ app/src/main/java/com/example/kirkasvalolamppu/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.kirkasvalolamppu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigation.setOnNavigationItemSelectedListener (mOnNavigationItemSelectedListener)
+
+        if (savedInstanceState == null) {
+            val fragment = AlarmFragment()
+            supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                .commit()
+        }
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+        when (menuItem.itemId) {
+            R.id.action_wakeup -> {
+                val fragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.action_turnOn -> {
+                val fragment = TurnOnFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+}
