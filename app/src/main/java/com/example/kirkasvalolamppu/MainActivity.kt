0287c725ef2378231a1c562205566e47d9a2dd1e
Kati Heikkinen <kati.a.heikkinen@gmail.com>
2019-08-03 22:14:39 +0300

    Added Wifi Broadcastreceivers

--- app/src/main/java/com/example/kirkasvalolamppu/MainActivity.kt
+++ app/src/main/java/com/example/kirkasvalolamppu/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.kirkasvalolamppu
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.IntentFilter
+import android.net.wifi.p2p.WifiP2pManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val manager: WifiP2pManager? by lazy(LazyThreadSafetyMode.NONE) {
+        getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager?
+    }
+    var mChannel: WifiP2pManager.Channel? = null
+    var receiver: BroadcastReceiver? = null
+    val intentFilter = IntentFilter().apply {
+        addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +33,27 @@ class MainActivity : AppCompatActivity() {
             val fragment = AlarmFragment()
             supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
                 .commit()
+        }
+
+        mChannel = manager?.initialize(this, mainLooper, null)
+        mChannel?.also { channel ->
+            receiver = WiFiDirectBroadcastReceiver(manager!!, channel, this)
+        }
+    }
+
+    /* register the broadcast receiver with the intent values to be matched */
+    override fun onResume() {
+        super.onResume()
+        receiver?.also { receiver ->
+            registerReceiver(receiver, intentFilter)
+        }
+    }
+
+    /* unregister the broadcast receiver */
+    override fun onPause() {
+        super.onPause()
+        receiver?.also { receiver ->
+            unregisterReceiver(receiver)
         }
     }
 
